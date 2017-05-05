58c1e04f23a3aad755abb29c66d5cb650599b948
Ali Soliman <atestsoliman@gmail.com>
2017-05-05 15:55:12 -0500

    SPA Routing with x-templates: allow switching between views with out loading any data from the server, but does't create DOM elements until they're needed. Defaults to login if there is no token stored.

--- src/setbgl.js
+++ src/setbgl.js
@@ -1,59 +1,78 @@
-//IIFE start the client side router
+//IIFE start the client side router BEFORE the DOM is ready
 (function () {
-    page('/log', function(ctx, next) {
+    page('/', logIn, function(ctx, next) {
         ctx.handled = true;
-        navToLog();
-        next();
+        render("landing");
     });
-    page('/graph', function(ctx, next) {
+    page('/log', logIn, function(ctx, next) {
         ctx.handled = true;
-        navToGraph();
-        next();
+        render("log");
     });
-
-    function navToLog() {
-        $(".content").addClass("hide");
-        $("<p>LOG PAGE PLACE HOLDER</p>").insertAfter(".navBar");
+    page('/graph', logIn, function(ctx, next) {
+        ctx.handled = true;
+        render("graph");
+    });
+    //renders login page if not logged in
+    //otherwise falls through to next.
+    function logIn(ctx, next) {
+        //when not logged in
+        if(!sessionStorage.getItem("token")){
+            //render the static html of logIn
+            render("logIn");
+            //add a listender to the submit button to do ajax
+            addLogInListener(next);
+        }
+        else {
+            next();
+        }
     }
-    function navToGraph() {
-        $(".content").addClass("hide");
-        $("<p>GRAPH PAGE PLACE HOLDER</p>").insertAfter(".navBar");
+    
+    function render(view) {
+        $(".content").html(template(view));
+    }
+    //from page examples/chrome
+    function template(name) {
+        return $("#" + name + '-template').html();
     }
 
+    function addLogInListener(next) {
+    //add listener to submit button
+        $(".submit").click(function(){
+            $.ajax({
+                url: "https://www.managebgl.com/api/1.0/login.json",
+                
+                data: { email: $(".email").val(),
+                        password: $(".password").val(),
+                        debug: ""
+                    },
+                type: "GET",
+
+                dataType: "json"
+
+            })
+            .done(  function(res) {
+                //if predict bgl accepts the login,
+                //store the login token in sessionStorage 
+                if(res.message === "Ok")  {
+                    sessionStorage.setItem("token", res.token);
+                    //safe to go though to next router
+                    next();
+                }
+                //if predictbgl rejects the login alert with the errors message
+                else {
+                    alert(res.message);
+                }
+            })
+            //if the request fails log out the error and status
+            .fail( function (xhr, status, errorThrown) {
+                    alert("Failed to connect to log in server");
+                    console.log( "Error: " + errorThrown );
+                    console.log( "Status: " + status );
+                    console.dir( xhr );
+            });
+        });
+    }
+
+    //start the client-side router
     page.start();
 })();
-$(document).ready(function() {
-    //add listener to submit button
-    $(".submit").click(function(){
-        $.ajax({
-            url: "https://www.managebgl.com/api/1.0/login.json",
-            
-            data: { email: $(".email").val(),
-                    password: $(".password").val(),
-                    debug: ""
-                },
-            type: "GET",
-
-            dataType: "json"
-
-        })
-        .done(  function(res) {
-            //if predict bgl accepts the login,
-            //store the login token in sessionStorage 
-            if(res.message === "Ok")  {
-                sessionStorage.setItem("token", res.token);
-            }
-            //if predictbgl rejects the login alert with the errors message
-            else {
-                alert(res.message);
-            }
-        })
-        //if the request fails log out the error and status
-        .fail( function (xhr, status, errorThrown) {
-                alert("Failed to connect to log in server");
-                console.log( "Error: " + errorThrown );
-                console.log( "Status: " + status );
-                console.dir( xhr );
-        });
-    });
-});
